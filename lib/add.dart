766890e08059a1f99c24c54f27b4b9fce8453417
TagidickTagidick <49733518+TagidickTagidick@users.noreply.github.com>
2020-06-06 12:30:55 +0300

    New bottom navigation bar (#3)
    * Добавил bottom navigation bar
    
    * New bottom navigation bar
    
    Co-authored-by: TagidickTagidick <yarinmaru13@mail.ru>

--- lib/add.dart
+++ lib/add.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:opinionleader/bottom_navigation_bar.dart';
 
 class Add extends StatefulWidget {
+  Add({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
   @override
   _AddState createState() => _AddState();
 }
 
 class _AddState extends State<Add> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        alignment: Alignment.bottomCenter,
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              // ваш код здесь, господа
-            ],
-          ),
-          NavBar(),
-        ],
-      ),
+
     );
   }
 }
--- lib/profile.dart
+++ lib/profile.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:opinionleader/bottom_navigation_bar.dart';
 
 class Profile extends StatefulWidget {
+  Profile({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
   @override
   _ProfileState createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        alignment: Alignment.bottomCenter,
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              // ваш код здесь, господа
-            ],
-          ),
-          NavBar(),
-        ],
-      ),
+
     );
   }
 }
