4797f822b408dc19c5aeca994151ef375ced03a5
Pavel <pavivin@yandex.ru>
2020-06-07 07:56:15 +0300

    news

--- lib/data.dart
+++ lib/data.dart
@@ -4,29 +4,31 @@ import 'package:opinionleader/post/comment.dart';
 import 'package:opinionleader/user.dart';
 import 'package:opinionleader/area/area.dart';
 
-User u1 = User('Вениамин Манукян',  'https://sun9-40.userapi.com/c857424/v857424894/dfe29/l2B0jorKZwE.jpg', [], [], [], [], true);
-User u2 = User('Вениамин Манукян',  'https://sun9-40.userapi.com/c857424/v857424894/dfe29/l2B0jorKZwE.jpg', [], [], [], [], true);
-User u3 = User('Алексей Сенников',  'https://sun1-47.userapi.com/TBbpGY77mvjvPq72Eq93P3QxzjDGwqDwUTVAuA/kzJFfZQqfG0.jpg', [], [], [], [], true);
-User u4 = User('Павел Ивин',  'https://sun9-23.userapi.com/c856128/v856128227/10f6e8/Uxh_Bx12Gmc.jpg', [], [], [], [], true);
-User u5 = User('Дык Фам',  'https://sun9-39.userapi.com/c855016/v855016549/15211c/MaKaeM0c2-k.jpg', [], [], [], [], true);
+User u1 = User('Вениамин Манукян',  'https://clck.ru/NrkhN', [], [], [], [], true);
+User u2 = User('Вениамин Манукян',  'https://clck.ru/NrkhN', [], [], [], [], true);
+User u3 = User('Алексей Сенников',  'https://clck.ru/Nrkhg', [], [], [], [], true);
+User u4 = User('Павел Ивин',  'https://clck.ru/Nrki3', [], [], [], [], true);
+User u5 = User('Дык Фам',  'https://clck.ru/NrkiH', [], [], [], [], true);
 Comment com = Comment(u1, "This was amazing!", DateTime.now(), false);
-Post p1 = Post('https://avatars.mds.yandex.net/get-altay/2714499/2a00000171061a2a8645b485942e1e845582/XXL',
+Post p1 = Post('https://clck.ru/Nrkgm',
     u1, 'Идея', DateTime.now(), [], [com],false, false, 2, 'Октябрьский район', 'Постройка нового детсада', Colors.orangeAccent, '#Новости');
-Post p2 = Post('https://avatars.mds.yandex.net/get-altay/2714499/2a00000171061a2a8645b485942e1e845582/XXL',
+Post p2 = Post('https://clck.ru/Nrkgm',
     u2, 'Замысел', DateTime.now(), [], [com],false, false, 1, 'Ленинский район', 'Облагораживание двора', Colors.orangeAccent, '#Предложение');
-Post p3 = Post('https://avatars.mds.yandex.net/get-altay/2714499/2a00000171061a2a8645b485942e1e845582/XXL',
-    u3, 'Замысел', DateTime.now(), [], [com],false, false, 1, 'Ленинский район', 'Облагораживание двора', Colors.orangeAccent, '#Новости');
-Post p4 = Post('https://avatars.mds.yandex.net/get-altay/2714499/2a00000171061a2a8645b485942e1e845582/XXL',
-    u4, 'Замысел', DateTime.now(), [], [com],false, false, 1, 'Ленинский район', 'Облагораживание двора', Colors.orangeAccent, '#Новости');
-Post p5 = Post('https://avatars.mds.yandex.net/get-altay/2714499/2a00000171061a2a8645b485942e1e845582/XXL',
-    u5, 'Замысел', DateTime.now(), [], [com],false, false, 1, 'Ленинский район', 'Облагораживание двора', Colors.orangeAccent, '#Новости');
+Post p3 = Post('https://clck.ru/Nrkgm',
+    u3, 'Замысел', DateTime.now(), [], [com],false, false, 1, 'Индустриальный район', 'Очистка пляжей', Colors.orangeAccent, '#Новости');
+Post p4 = Post('https://clck.ru/Nrkgm',
+    u4, 'Замысел', DateTime.now(), [], [com],false, false, 1, 'Железнодорожный район', 'Уборка несанционированных свалок', Colors.orangeAccent, '#Новости');
+Post p5 = Post('https://clck.ru/Nrkgm',
+    u5, 'Замысел', DateTime.now(), [], [com],false, false, 1, 'Центральный район', 'Посадка цветов', Colors.orangeAccent, '#Новости');
 
 
 List<Post> posts = [p1, p2, p3, p4, p5];
 List<Post> userPosts = [p1, p2];
 
-Area a1 = Area('Ленинский район', '75%', 'https://barnaul.org/upload/resize_cache/iblock/470/945_527_2/DSC_1520.jpg', Colors.green, 'Ленинский район');
-Area a2 = Area('Октябрьский район', '50%', 'https://barnaul.org/upload/iblock/b62/dsc_0006.jpg', Colors.blue, 'Октябрьский район');
-List<Area> areas = [a1,a2];
+Area a1 = Area('Ленинский район', '77%', 'https://clck.ru/NrkgD', Colors.green, 'Ленинский район');
+Area a2 = Area('Октябрьский район', '65%', 'https://clck.ru/NrkgR', Colors.blue, 'Октябрьский район');
+Area a3 = Area('Индустриальный район', '62%', 'https://clck.ru/Nrkjc', Colors.green, 'Индустриальный район');
+Area a4 = Area('Центральный район', '42%', 'https://clck.ru/NrkpZ', Colors.green, 'Центральный район');
+List<Area> areas = [a1,a2, a3, a4];
 //Post('https://clck.ru/N8Wo6', u2, 'Замысел', DateTime.now(), [u1,u2,u3], [com],true, false );
 //Post('https://clck.ru/N8Wo6', u3, 'хм', DateTime.now(), [u1,u2,u3], [com],true, false );
